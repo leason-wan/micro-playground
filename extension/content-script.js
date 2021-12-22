f4477e1b94d71d25787272b2720e3ab19dbd454e
lixing <lixing@webuy.ai>
2021-12-22 20:23:11 +0800

    first commit

---
+++ extension/content-script.js
@@ -0,0 +1,5 @@
+// document.body.style.backgroundColor = 'orange';
+// localStorage.setItem('extension', "test from extension");
+// const extension = localStorage.getItem("extension");
+
+// console.log(`[phoenix-debugger]: ${extension}`);
---
+++ extension/devtools-background.js
@@ -0,0 +1,12 @@
+chrome.devtools.panels.create(
+  'Phoenix', 'icons/128.png', 'devtools.html',
+  panel => {
+    // panel loaded
+    // panel.onShown.addListener(onPanelShown)
+    // panel.onHidden.addListener(onPanelHidden)
+
+    // const l = localStorage.getItem('PHOENIXDEBUG');
+    // console.log('localStorage', l);
+    // console.log('window---------------', window.HOOK);
+  },
+)
